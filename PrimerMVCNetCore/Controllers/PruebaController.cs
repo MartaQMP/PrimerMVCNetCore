79741e9d2845093d4fc1a4f3d18d6e68e0e3499d
MartaQMP <martaquirosmp@gmail.com>
2026-01-21 14:19:32 +0100

    21/01 dia

---
+++ PrimerMVCNetCore/Controllers/PruebaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMVCNetCore.Controllers
+{
+    public class PruebaController : Controller
+    {
+        public IActionResult PrimeraPagina()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
